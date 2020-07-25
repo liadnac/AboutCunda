33ff88bb06b9d484124bcfda7a9b6b65e84b2324
Liad <liad.nachlai@gmail.com>
2020-07-26 00:01:53 +0300

    Added real photos, line break in the bio and data binding.

--- app/src/main/java/sh/deut/aboutcunda/MainActivity.kt
+++ app/src/main/java/sh/deut/aboutcunda/MainActivity.kt
@@ -1,15 +1,32 @@
 package sh.deut.aboutcunda
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import sh.deut.aboutcunda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
-    fun FavFace (view: View){
 
+    fun favFace(view: View) {
+        binding.apply {
+            favFace.text = binding.favFaceEdit.text
+            invalidateAll()
+            binding.favFaceEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            binding.favFace.visibility = View.VISIBLE
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
